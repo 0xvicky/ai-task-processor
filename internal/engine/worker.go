7c2bac7e5e08c5d1d3ba9d5ec0c43df45b84bcef
vicky <tyagivivek528@gmail.com>
2026-04-12 21:38:50 +0530

    wait group workarounds

--- internal/engine/worker.go
+++ internal/engine/worker.go
@@ -1,10 +1,17 @@
 package engine
 
-import "ai-task-processor/internal/repository/task"
+import (
+	"ai-task-processor/internal/repository/task"
+	"sync"
+)
 
 func StartEngine(tRepo task.TaskRepository, nWorkers int) {
 	//spawn the workers
+	wgu * sync.WaitGroup
 
+	for i := 0; i < nWorkers; i++ {
+
+	}
 	//each worker polls db to check for pending tasks
 
 	//if found then safely lock it and update the state to running and if not found, sleep
